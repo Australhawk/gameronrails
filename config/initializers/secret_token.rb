e07d5b23dc53becb9a1e38ae442e71a6c7fc154e
australhawk <austral@gamerdeal.tv>
2013-05-25 04:35:58 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GamerOnRails::Application.config.secret_token = 'feba51e41673c5d7180102ac4f5fd7fc1e178440ac1da0635aab0c8aa89a6d7d86d02a54eb846aab114397ae4e6b3c3c1d89b8bf1913d500515f4366d763ba67'
