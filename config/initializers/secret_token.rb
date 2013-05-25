ff32b4cafad33fae36493c8381532b77bd55fc54
australhawk <austral@gamerdeal.tv>
2013-05-25 04:45:53 -0400

    Secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-GamerOnRails::Application.config.secret_token = 'feba51e41673c5d7180102ac4f5fd7fc1e178440ac1da0635aab0c8aa89a6d7d86d02a54eb846aab114397ae4e6b3c3c1d89b8bf1913d500515f4366d763ba67'
+GamerOnRails::Application.config.secret_token = '0fe7b7f35d0b71b8577faa072fcf6a4fc0c473f9bd3cafa288903e5ccb6d76cc3d7945a9dc41604956a031b2cca7945773b0a6e11b460c50370dad93dcb8423a'
